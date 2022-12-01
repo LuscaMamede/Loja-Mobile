6495528c12e8b875c722a4ed007c184ac8b62684
luscamamede <eumesmolucassilva@gmail.com>
2022-12-01 14:23:36 -0300

    modified:   lib/pages/products_overview_page.dart

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,25 +1,43 @@
-// ignore_for_file: sort_child_properties_last
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_grid.dart';
+import 'package:shop/models/product_list.dart';
+
+enum FilterOptions {
+  favorite,
+  all,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
         actions: [
-          PopupMenuButton(itemBuilder:(_) => [
-            const PopupMenuItem(
-            child: Text('Somente Favoritos'),
-            value: 0, 
-            ),
-            const PopupMenuItem(child: Text('Todos'),value: 1,)
-            ),
-          ],
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorite,
+                child: Text('Somente Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
           ),
         ],
       ),
