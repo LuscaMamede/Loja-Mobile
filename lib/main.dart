5e4d3fdc7efd019794bf8b9cdad81ca83c48fe53
luscamamede <eumesmolucassilva@gmail.com>
2022-12-01 13:31:51 -0300

    Flutter-Shop

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Minha Loja',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato',
+        ),
+        home: const ProductsOverviewPage(),
+        routes: {
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
+
+import '../components/product_grid.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  const ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+      ),
+      body: const ProductGrid(),
+    );
+  }
+}
