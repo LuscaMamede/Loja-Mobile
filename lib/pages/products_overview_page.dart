c11b1309dca5e9d67ff884241bae9ff1da965d4b
luscamamede <eumesmolucassilva@gmail.com>
2022-12-01 14:11:17 -0300

    modified:   lib/pages/products_overview_page.dart

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
 import 'package:shop/components/product_grid.dart';
 
@@ -9,6 +11,17 @@ class ProductsOverviewPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(itemBuilder:(_) => [
+            const PopupMenuItem(
+            child: Text('Somente Favoritos'),
+            value: 0, 
+            ),
+            const PopupMenuItem(child: Text('Todos'),value: 1,)
+            ),
+          ],
+          ),
+        ],
       ),
       body: const ProductGrid(),
     );
