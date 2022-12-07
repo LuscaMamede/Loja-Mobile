00a3df5c6ca068289c8ec7eb47105e597bbaec4e
luscamamede <eumesmolucassilva@gmail.com>
2022-12-07 14:43:20 -0300

    Changes to be committed: 	modified:   lib/main.dart 	new file:   lib/pages/auth_page.dart 	modified:   lib/pages/product_form_page.dart 	modified:   lib/utils/app_routes.dart

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: no_leading_underscores_for_local_identifiers, use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
