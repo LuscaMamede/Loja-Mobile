2e0c6091bc5a6e45d028acb16ffb426da04b707b
luscamamede <eumesmolucassilva@gmail.com>
2022-12-08 16:06:40 -0300

    new file:   .vs/CMakeWorkspaceSettings.json 	new file:   .vs/ProjectSettings.json 	new file:   .vs/VSWorkspaceState.json 	new file:   .vs/shop/FileContentIndex/a15d01a5-1820-4ffb-a9c4-25a01ba22a05.vsidx 	new file:   .vs/shop/FileContentIndex/e8876fda-b04b-4632-8a0b-5831897444f4.vsidx 	new file:   .vs/shop/FileContentIndex/read.lock 	new file:   .vs/shop/v17/.wsuo 	new file:   .vs/shop/v17/Browse.VC.db 	new file:   .vs/slnx.sqlite 	modified:   lib/components/app_drawer.dart 	new file:   lib/components/auth_form.dart 	modified:   lib/components/product_grid_item.dart 	new file:   lib/exceptions/auth_exception.dart 	modified:   lib/main.dart 	new file:   lib/models/auth.dart 	modified:   lib/models/order_list.dart 	modified:   lib/models/product.dart 	modified:   lib/models/product_list.dart 	new file:   lib/pages/auth_or_home_page.dart 	modified:   lib/pages/auth_page.dart 	modified:   lib/pages/product_form_page.dart 	modified:   lib/utils/app_routes.dart 	modified:   lib/utils/constants.dart

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,8 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:shop/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -27,17 +26,21 @@ class AuthPage extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-                  //cascade operator ..
+                  margin: const EdgeInsets.only(bottom: 20),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 70,
+                  ),
+                  // cascade operator
                   transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     color: Colors.deepOrange.shade900,
-                    boxShadow: [
+                    boxShadow: const [
                       BoxShadow(
                         blurRadius: 8,
                         color: Colors.black26,
@@ -45,14 +48,16 @@ class AuthPage extends StatelessWidget {
                       ),
                     ],
                   ),
-                  child: Text(
+                  child: const Text(
                     'Minha Loja',
                     style: TextStyle(
-                        fontSize: 45,
-                        fontFamily: 'Anton',
-                        color: Theme.of(context).textTheme.headline6?.color),
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color: Colors.white,
+                    ),
                   ),
                 ),
+                const AuthForm(),
               ],
             ),
           ),
@@ -61,3 +66,16 @@ class AuthPage extends StatelessWidget {
     );
   }
 }
+
+// Exemplo usado para explicar o cascade operator
+// void main() {
+//   List<int> a = [1, 2, 3];
+//   a.add(4);
+//   a.add(5);
+//   a.add(6);
+
+//   // cascade operator!
+//   a..add(7)..add(8)..add(9);
+
+//   print(a);
+// }
